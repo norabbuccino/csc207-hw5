da69980d7daffdde40cd1ab45efea00ccfbc6a20
norabbuccino <bresette@grinnell.edu>
2014-03-05 21:11:23 -0600

    added tests for exponentiation

--- src/ExponentiationTest.java
+++ src/ExponentiationTest.java
@@ -7,9 +7,22 @@ public class ExponentiationTest
 {
 
   @Test
-  public void test()
+  public void testPositive()
   {
-    fail("Not yet implemented");
+    assertEquals("2^3 should be 8: ", 8, Exponentiation.expt(2, 3), .0001);
+  }
+  
+  @Test
+  public void testZero()
+  {
+    assertEquals("anything raised to the zero is 1", 1, Exponentiation.expt(103, 0), .0001);
+  }
+  
+  @Test
+  public void testNegative()
+  {
+    assertEquals("a negative raised to an even power will be even", 4, Exponentiation.expt(-2, 2), .0001);
+    assertEquals("a negative raised to an odd power will be odd", -8, Exponentiation.expt(-2, 3), .0001);
   }
 
 }
